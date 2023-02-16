83928776ed16ad2f5f466d3ade2bf1b6686e6dcb
Jack Sanger <89049086+You-Acorn@users.noreply.github.com>
2023-02-16 09:43:59 -0800

    Update Movie.java

--- src/main/java/com/perscholas/RestApi_Group4/repository/entities/Movie.java
+++ src/main/java/com/perscholas/RestApi_Group4/repository/entities/Movie.java
@@ -1,4 +1,16 @@
 package com.perscholas.RestApi_Group4.repository.entities;
 
-public class Movie {
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie implements Serializable {
+	private String title, year, rated, released, runtime, genre, director, writer, actors, plot, language, country, awards, poster, ratings;
 }

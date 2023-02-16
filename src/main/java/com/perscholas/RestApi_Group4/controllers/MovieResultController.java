5caede47be7947daec31ad1fd443886709bdb55b
Jack Sanger <89049086+You-Acorn@users.noreply.github.com>
2023-02-16 11:46:14 -0800

    Update MovieResultController.java

--- src/main/java/com/perscholas/RestApi_Group4/controllers/MovieResultController.java
+++ src/main/java/com/perscholas/RestApi_Group4/controllers/MovieResultController.java
@@ -1,11 +1,13 @@
 package com.perscholas.RestApi_Group4.controllers;
 
 
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 import com.perscholas.RestApi_Group4.repository.entities.Movie;
 
@@ -13,8 +15,39 @@ import com.perscholas.RestApi_Group4.repository.entities.Movie;
 @RequestMapping("/movieinfo")
 public class MovieResultController {
     @PostMapping
-    public String postHandler(Model model) {
-    	model.addAttribute("myMovie", new Movie());
+    public String postHandler(Model model, Movie myMovie) {
+    	model.addAttribute("myMovie", myMovie);
+    	
+    	String url = "http://www.omdbapi.com/?t=";
+    	url += myMovie.getTitle().replaceAll(" ", "+");
+    	url +="&apikey=b79fdda2";
+    	
+    	RestTemplate restTemplate = new RestTemplate();
+    	String movieData = restTemplate.getForObject(url, String.class);
+    	
+    	JSONObject json = new JSONObject(movieData);
+    	myMovie.setYear(json.getString("Year"));
+    	myMovie.setRated(json.getString("Rated"));
+    	myMovie.setReleased(json.getString("Released"));
+    	myMovie.setRuntime(json.getString("Runtime"));
+    	myMovie.setGenre(json.getString("Genre"));
+    	myMovie.setDirector(json.getString("Director"));
+    	myMovie.setWriter(json.getString("Writer"));
+    	myMovie.setActors(json.getString("Actors"));
+    	myMovie.setPlot(json.getString("Plot"));
+    	myMovie.setLanguage(json.getString("Language"));
+    	myMovie.setCountry(json.getString("Country"));
+    	myMovie.setAwards(json.getString("Awards"));
+    	myMovie.setPoster(json.getString("Poster"));
+    	
+    	myMovie.setRatings("No ratings");
+    	
+    	for(int i=0; i< json.getJSONArray("Ratings").length(); i++) {
+    		JSONObject rating = new JSONObject(json.getJSONArray("Ratings").get(i).toString());
+    		if(rating.getString("Source").compareToIgnoreCase("Rotten Tomatoes") == 0) {
+    			myMovie.setRatings(rating.getString("Value"));
+    		}
+    	}
         return "movieinfo";
     }
 

9c506708d9fb597beb8149fea2b8ffb2993c0779
chaid-s <chai.d.sean@gmail.com>
2023-02-16 11:53:20 -0600

    Stuff to fetch Movie Name
    Basic form to submit a movie name and changes in MovieResultController to receive user input.

--- src/main/java/com/perscholas/RestApi_Group4/controllers/MovieResultController.java
+++ src/main/java/com/perscholas/RestApi_Group4/controllers/MovieResultController.java
@@ -1,16 +1,20 @@
 package com.perscholas.RestApi_Group4.controllers;
 
-import ch.qos.logback.core.model.Model;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.perscholas.RestApi_Group4.repository.entities.Movie;
 
 @Controller
 @RequestMapping("/movieinfo")
 public class MovieResultController {
     @PostMapping
     public String postHandler(Model model) {
+    	model.addAttribute("myMovie", new Movie());
         return "movieinfo";
     }
 

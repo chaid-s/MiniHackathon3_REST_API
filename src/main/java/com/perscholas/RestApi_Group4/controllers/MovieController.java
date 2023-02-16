9ab1ecc56dde9a0283a75dda9ff318a918217b0d
cspadav1 <Christen.Spadavecchia1@T-Mobile.com>
2023-02-16 12:17:00 -0800

    Added Movie to model in MovieController

--- src/main/java/com/perscholas/RestApi_Group4/controllers/MovieController.java
+++ src/main/java/com/perscholas/RestApi_Group4/controllers/MovieController.java
@@ -1,6 +1,8 @@
 package com.perscholas.RestApi_Group4.controllers;
 
+import com.perscholas.RestApi_Group4.repository.entities.Movie;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class MovieController {
     @GetMapping
-    public String getMoviePage() {
+    public String getMoviePage(Model model) {
+        model.addAttribute("myMovie",new Movie());
         return "movies";
     }
 }

5f9d24f7ade1c64b2867e1d8d47d09a68ffe6f15
cspadav1 <Christen.Spadavecchia1@T-Mobile.com>
2023-02-16 09:06:09 -0800

    Changed MovieController and MovieResultController to use @Controller instead of @RestController

--- src/main/java/com/perscholas/RestApi_Group4/controllers/MovieResultController.java
+++ src/main/java/com/perscholas/RestApi_Group4/controllers/MovieResultController.java
@@ -1,12 +1,12 @@
 package com.perscholas.RestApi_Group4.controllers;
 
 import ch.qos.logback.core.model.Model;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/movieinfo")
 public class MovieResultController {
     @PostMapping
